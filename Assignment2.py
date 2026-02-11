e6f04a953d30b0969c012fbd87893cc15975e530
kunal-kalamkar77 <kunal.1251100220@vit.edu>
2026-02-11 18:29:52 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,1 +1,8 @@
-
+#Problem statement - program to swaptwo variables
+#We will assign variables 1st
+a = float(input("Enter any value :"))
+b = float(input("Enter 2nd value :"))
+temp = a
+a = b
+b = temp
+print("Variables after swapping are :",(a,b))
