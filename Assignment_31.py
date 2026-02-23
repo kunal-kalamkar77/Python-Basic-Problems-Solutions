b985a5d7421cd5e3c635d10e267a6f35200bcced
kunal-kalamkar77 <kunal.1251100220@vit.edu>
2026-02-23 22:46:06 +0530

    Add program to sum and repeat two lists

---
+++ Assignment_31.py
@@ -0,0 +1,9 @@
+#Problem Statement - Program to add two Lists 
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+list3 = list1 + list2
+list4 = list1 * 2
+print("The first list is: ", list1)
+print("The second list is: ", list2)
+print("The sum of the two lists is: ", list3)
+print("The first list repeated twice is: ", list4)
