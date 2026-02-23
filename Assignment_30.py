ff0b930eef1a8abb040554b04fbad9308e6e8766
kunal-kalamkar77 <kunal.1251100220@vit.edu>
2026-02-23 22:41:46 +0530

    Add program to print list indices and values
    Implement a program to access and print list indices and values.

---
+++ Assignment_30.py
@@ -0,0 +1,4 @@
+#Problem Statement -  Create a program to access list index and values.
+l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+for index in range(len(l)):
+    print("Index: ", index, "Value: ", l[index])
