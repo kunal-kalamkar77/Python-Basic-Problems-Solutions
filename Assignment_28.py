2ee085f3daa0fda46514b67f4fad5a7ea8b5ae7d
kunal-kalamkar77 <kunal.1251100220@vit.edu>
2026-02-23 22:34:24 +0530

    Implement list range viewer in Assignment_28.py
    Added a program to display list elements in specified ranges.

---
+++ Assignment_28.py
@@ -0,0 +1,6 @@
+#Problem Statement - Create a program to view elements of a list in a specified range. The program should take the list and the range as input from the user.
+l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+print(l)
+print(l[0:4])
+print(l[4:8])
+print(l[0:8:3])
