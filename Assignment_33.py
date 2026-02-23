5d790c1d09a07b8c7ad10db9383776a8ff174eb7
kunal-kalamkar77 <kunal.1251100220@vit.edu>
2026-02-23 22:53:38 +0530

    Add program to find largest and lowest numbers
    Implemented a program to find the largest and lowest numbers in a list.

---
+++ Assignment_33.py
@@ -0,0 +1,6 @@
+#Problem Statement - Create a program to find largest number & lowest number in list.
+list = input("Enter a list of numbers : ").split()
+largest = max(list)
+lowest = min(list)
+print("The largest number in the list is: ", largest)
+print("The lowest number in the list is: ", lowest)
