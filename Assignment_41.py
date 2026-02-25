1c17c816928c29b2a0210d0d128688a7c5a0016d
kunal-kalamkar77 <kunal.1251100220@vit.edu>
2026-02-25 17:00:01 +0530

    Add program to print all values of a dictionary

---
+++ Assignment_41.py
@@ -0,0 +1,11 @@
+#Problem Statement: Create a program to print all values of dictionary.
+data = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York",
+    "education": "Bachelor's Degree",
+    "profession": "Software Engineer"
+}
+print("Values in the Dictionary:")
+for value in data.values():
+    print(value)
