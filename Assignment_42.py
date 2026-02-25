555824726cc1b9bd4186142392c92f00173d243e
kunal-kalamkar77 <kunal.1251100220@vit.edu>
2026-02-25 17:04:44 +0530

    Add program to print dictionary keys

---
+++ Assignment_42.py
@@ -0,0 +1,9 @@
+#Problem Statement: Create a program to print the keys of a dictionary.
+dict ={
+    "name" : "Kunal",
+    "Age" : 22,
+    "City" : "Pune",
+    "Education" : "B.Tech",
+    "Profession" : "Student"
+}
+print(dict.keys())
