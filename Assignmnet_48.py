fe101f8f00d9418d9e742b405fac0a5409954c0e
kunal-kalamkar77 <kunal.1251100220@vit.edu>
2026-02-25 17:24:40 +0530

    Add program to find max and min in a dictionary
    This program calculates and prints the maximum and minimum values from a given dictionary.

---
+++ Assignmnet_48.py
@@ -0,0 +1,13 @@
+#Problem statement :Program to get the maximum and minimum value in a dictionary
+dict = {
+    "a": 10,
+    "b": 20,
+    "c": 5,
+    "d": 15,
+    "e": 25
+}
+# Get maximum and minimum values
+max_value = max(dict.values())
+min_value = min(dict.values())
+print("Maximum value in the dictionary:", max_value)
+print("Minimum value in the dictionary:", min_value)
